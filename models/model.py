f4fbd2ebcd4a6868e1ce1a51a7cde2c17689a7d2
theuag <135053728+theuag@users.noreply.github.com>
2024-10-21 15:32:36 -0300

    Update model.py

--- models/model.py
+++ models/model.py
@@ -3,5 +3,8 @@ class Login:
         self.usuario = usuario
         self.__senha__ = senha
 
+    def getSenha(self):
+        return self.__senha__
 
-logado= Login("matheus",1234)
+
+logado= Login("matheus","1234")
